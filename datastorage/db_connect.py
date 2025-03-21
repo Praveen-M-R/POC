16392b70f3c66e05f57a9bbab8bd5733ae6f987e
Praveen R <praveen.ramesh@polynomial.ai>
2025-03-21 04:39:16 -0700

    Add Firebase integration

--- datastorage/db_connect.py
+++ datastorage/db_connect.py
@@ -1,7 +1,8 @@
 import firebase_admin
 from firebase_admin import credentials, firestore
 from core.config import FIREBASE_CREDENTIALS_PATH 
-
+import json
+import os
 # Initialize Firebase
 cred = credentials.Certificate(FIREBASE_CREDENTIALS_PATH)
 firebase_admin.initialize_app(cred)
@@ -16,3 +17,10 @@ def save_student_report(report):
     doc_ref = reports_collection.add(report)
     report["_id"] = doc_ref[1].id  # Firestore auto-generates an ID
     return report
+
+if not firebase_admin._apps:
+    firebase_creds = os.getenv("FIREBASE_CREDS")
+    cred_dict = json.loads(firebase_creds)
+    cred = credentials.Certificate(cred_dict)
+    firebase_admin.initialize_app(cred)
+db = firestore.client()
